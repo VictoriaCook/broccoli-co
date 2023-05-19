7991622e43a3dbfed090e301465f549f2f8bb1d6
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-05-19 16:03:31 +1000

    add accent colour to title text

--- broccoli-co/app/components/Title/Title.tsx
+++ broccoli-co/app/components/Title/Title.tsx
@@ -3,7 +3,7 @@ import styles from './Title.module.css';
 
 const Title: React.FC = () => {
     return (
-        <h1 className={styles.titleStyles}>A better way <br /> to enjoy every day.</h1>
+        <h1 className={styles.titleStyles}>A <span className={styles.titleAccentStyles}>better</span> way <br /> to enjoy every day.</h1>
     );
 };
 
