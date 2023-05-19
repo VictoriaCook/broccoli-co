61e8aeb6c8d19f334ff1a3d153eb269290c0b8c2
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-05-19 15:54:27 +1000

    add invitation success modal styling

--- broccoli-co/app/components/InvitationSuccessModal/InvitationSuccessModal.tsx
+++ broccoli-co/app/components/InvitationSuccessModal/InvitationSuccessModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Modal } from 'antd';
 import Button from '../Button/Button';
+import styles from './InvitationSuccessModal.module.css';
 
 interface InvitationSuccessModalProps {
   open: boolean;
@@ -10,10 +11,10 @@ interface InvitationSuccessModalProps {
 const InvitationSuccessModal: React.FC<InvitationSuccessModalProps> = ({ open, onHide }) => {
   return (
     <>
-      <Modal title="Basic Modal" open={open} onOk={onHide} onCancel={onHide}>
-        <div>
-          <h2>All done!</h2>
-          <p>You will be one of the first to experience Broccoli & Co. when we launch.</p>
+      <Modal open={open} onOk={onHide} onCancel={onHide}>
+        <div className={styles.successDivStyles}>
+          <h2 className={styles.successH2Styles}>All done!</h2>
+          <p className={styles.successTextStyles}>You will be one of the first to experience Broccoli & Co. when we launch.</p>
           <Button buttonText="OK" onClick={onHide} />
         </div>
       </Modal>
