cb7ca6d9710f2881d0197f0bcdffc978a39ddd4c
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-05-19 18:59:18 +1000

    clean up page.tsx

--- broccoli-co/app/page.tsx
+++ broccoli-co/app/page.tsx
@@ -7,30 +7,32 @@ import Button from './components/Button/Button';
 import BodyText from './components/BodyText/BodyText';
 import InvitationModal from './components/InvitationModal/InvitationModal';
 import InvitationSuccessModal from './components/InvitationSuccessModal/InvitationSuccessModal';
+import HighlightText from './components/HighlightText/HighlightText';
 
 export default function HomePage() {
   const [isInvitationModalOpen, setIsInvitationModalOpen] = useState(false);
   const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
 
   const handleSuccess = () => {
-    console.log('success');
     setIsInvitationModalOpen(false);
     setIsSuccessModalOpen(true);
   }
 
   return (
-    <div>
+    <>
       <Header />
-          <Title />
-          <BodyText />
-          <Button buttonText="Request an invite" onClick={() => setIsInvitationModalOpen(true)}/>
-        <Footer />
-        <InvitationModal 
+      <main>
+        <Title>A <HighlightText text="better"/> way to enjoy every day</Title>
+        <BodyText>Be the <HighlightText text="first"/> to know when we launch.</BodyText>
+        <Button buttonText="Request an invite" onClick={() => setIsInvitationModalOpen(true)}/>
+      </main>
+      <Footer />
+      <InvitationModal 
           open={isInvitationModalOpen}
           onHide={() => setIsInvitationModalOpen(false)}
           onSuccess={handleSuccess}
         />
         <InvitationSuccessModal open={isSuccessModalOpen} onHide={() => setIsSuccessModalOpen(false)}/>
-    </div>
+    </>
   );
 }
