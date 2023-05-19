b114c47725d809c8eea7c9bf82f51bc66b31e250
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-05-19 11:33:45 +1000

    create invitationmodal component and functionality

---
+++ broccoli-co/app/components/InvitationModal/InvitationModal.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import { Modal } from 'antd';
+import Form from '../Form/Form';
+import TextInput from '../TextInput/TextInput';
+import SubmitButton from '../SubmitButton/SubmitButton';
+import { areEmailsEqual } from '../../utils/areEmailsEqual/areEmailsEqual';
+import { isFullNameValid } from '../../utils/isFullNameValid/isFullNameValid';
+import { isEmailValid } from '../../utils/isEmailValid/isEmailValid';
+
+interface InvitationModalProps {
+  open: boolean;
+  onHide: () => void;
+  onSuccess: () => void;
+};
+
+const InvitationModal: React.FC<InvitationModalProps> = ({ open, onHide, onSuccess }) => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [confirmEmail, setConfirmEmail] = useState('');
+  const [validationError, setValidationError] = useState('');
+  const [serverError, setServerError] = useState('');
+  const [submitButtonText, setSubmitButtonText] = useState('Send');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'fullName') {
+      setFullName(inputValue);
+    }
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    }
+    if (inputType === 'confirmEmail') {
+      setConfirmEmail(inputValue);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!fullName || !email || !confirmEmail) {
+      setValidationError('Please complete all fields.');
+    } else if (!isFullNameValid(fullName)) {
+      setValidationError('Your full name must be at least three letters. Please try again.');
+    } else if (!isEmailValid(email)) {
+      setValidationError('Please enter a valid email address.');
+    } else if (!areEmailsEqual(email, confirmEmail)) {
+      setValidationError('Emails do not match. Please try again.');
+    } else {
+      setSubmitButtonText('Sending, please wait...');
+      try {
+        const response = await fetch('https://us-central1-blinkapp-684c1.cloudfunctions.net/fakeAuth', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name: fullName, email: email }),
+        });
+        const text = await response.text();
+        if (response.status === 200) {
+          onSuccess();
+        } else {
+          setServerError(text);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleOk = () => {
+    // setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    // setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Modal title="Basic Modal" open={open} onOk={handleOk} onCancel={onHide}>
+        <Form onSubmit={handleSubmit} formHeading="Request an invite">
+          <p>{validationError}</p>
+          <TextInput value={fullName} label="fullName" placeholder="Full name" onChange={handleInputChange} />
+          <TextInput value={email} label="email" placeholder="Email" onChange={handleInputChange} />
+          <TextInput value={confirmEmail} label="confirmEmail" placeholder="Confirm email" onChange={handleInputChange} />
+          <SubmitButton buttonText={submitButtonText} />
+        </Form>
+        <p>{serverError}</p>
+      </Modal>
+    </>
+  );
+};
+
+export default InvitationModal;
