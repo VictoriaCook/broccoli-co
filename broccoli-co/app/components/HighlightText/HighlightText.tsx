5c9d8e9bf50b3cf7349ab9f58c19e9e110d1635c
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-05-19 19:01:26 +1000

    create highlighttext component

---
+++ broccoli-co/app/components/HighlightText/HighlightText.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './HighlightText.module.css';
+
+interface HighlightTextProps {
+    text: string;
+}
+
+const HighlightText: React.FC<HighlightTextProps> = (props) => {
+    return (
+        <span className={styles.highlightTextStyles}>{props.text}</span>
+    );
+};
+
+export default HighlightText;
