a3edf142bfe60617c68aaa599e9d654fcfc5e305
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-05-19 19:01:05 +1000

    remove hardcoding and pass in text as children

--- broccoli-co/app/components/Title/Title.tsx
+++ broccoli-co/app/components/Title/Title.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './Title.module.css';
 
-const Title: React.FC = () => {
+interface TitleProps {
+    children: React.ReactNode;
+}
+
+const Title: React.FC<TitleProps> = (props) => {
     return (
-        <h1 className={styles.titleStyles}>A <span className={styles.titleAccentStyles}>better</span> way <br /> to enjoy every day.</h1>
+        <h1 className={styles.titleStyles}>{props.children}</h1>
     );
 };
 
