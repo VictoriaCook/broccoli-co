3e106c3c10a0d1b04464a1a8470068a993a40d20
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-05-21 11:42:14 +1000

    create test for submit button component

---
+++ broccoli-co/app/components/SubmitButton/SubmitButton.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import SubmitButton from './SubmitButton';
+
+describe('SubmitButton', () => {
+  test('renders button with correct text', () => {
+    const buttonText = 'Submit';
+    render(<SubmitButton buttonText={buttonText} />);
+    
+    const buttonElement = screen.getByText(buttonText);
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
