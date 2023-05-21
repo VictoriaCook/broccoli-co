3fbca84e4f88848485bd80bacffa797318986559
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-05-21 12:03:30 +1000

    create test for title component

---
+++ broccoli-co/app/components/Title/Title.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Title from './Title';
+
+it('renders the title correctly', () => {
+    // Arrange
+    const titleText = 'Hello, World!';
+  
+    // Act
+    const { getByText } = render(<Title>{titleText}</Title>);
+  
+    // Assert
+    const titleElement = getByText(titleText);
+    expect(titleElement).toBeInTheDocument();
+  });
+  
